7077412c138389ae2abe33c888a97048daba5c01
amu <amu.shun1992@gmail.com>
2025-07-07 12:08:40 +0530

    update chnges on port no

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,9 @@
-// backend/server.js
 const dotenv = require('dotenv');
 dotenv.config(); 
 
 const app = require('./app');
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Server running on port ${PORT}`);
-// });
-
-const port = process.env.PORT || 5000;
-app.listen(port, '0.0.0.0', () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
